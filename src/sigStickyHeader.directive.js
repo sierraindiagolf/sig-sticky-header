4c7446fd1bbe5b972ebd3ae5d684d4b0f419879c
Stefan <s.gencarau@gmail.com>
2015-11-17 21:09:21 +0200

    Add support for custom classes

--- src/sigStickyHeader.directive.js
+++ src/sigStickyHeader.directive.js
@@ -6,7 +6,7 @@
     function SigStickyHeader() {
         var directive;
 
-        function link(scope, element) {
+        function link(scope, element, attributes) {
             var originalProperties = element.css(['position', 'top', 'left', 'zIndex']);
             var initialBoundingClient = element[0].getBoundingClientRect();
             var positionFixedProperties = {
@@ -28,10 +28,25 @@
                     element.css(positionFixedProperties);
                     element.addClass('sig-sh-fixed');
                     element.removeClass('sig-sh-original');
+                    addClasses(attributes, 'Fixed');
                 } else {
                     element.css(originalProperties);
                     element.removeClass('sig-sh-fixed');
                     element.addClass('sig-sh-original');
+                    addClasses(attributes, 'Original');
+                }
+            }
+
+            function addClasses(attributes, position, addOrRemove) {
+                if (!addOrRemove) {
+                    var pos = position === 'Fixed' ? 'Original' : 'Fixed';
+                    addClasses(attributes, pos, element.removeClass);
+                }
+
+                addOrRemove = element.addClass;
+
+                if (attributes['shClasses' + position]) {
+                    addOrRemove(attributes['shClasses' + position]);
                 }
             }
 
